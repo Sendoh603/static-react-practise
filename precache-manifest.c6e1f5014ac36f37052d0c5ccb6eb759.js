77facdd20a7f1ad4eb6275d221c00ff66f4df245
Wei Zhang <sheepchina@hotmail.com>
2020-01-23 11:03:29 -0600

    Updates

---
+++ precache-manifest.c6e1f5014ac36f37052d0c5ccb6eb759.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87602d6ca169b3fc0e3507e4c67df5ea",
+    "url": "/Sendoh603/Static-react-practise2.git/index.html"
+  },
+  {
+    "revision": "9a3fe261e906d4a38908",
+    "url": "/Sendoh603/Static-react-practise2.git/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8af1c97d36f0b1426ae8",
+    "url": "/Sendoh603/Static-react-practise2.git/static/js/2.b6fabe4d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Sendoh603/Static-react-practise2.git/static/js/2.b6fabe4d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9a3fe261e906d4a38908",
+    "url": "/Sendoh603/Static-react-practise2.git/static/js/main.545514a0.chunk.js"
+  },
+  {
+    "revision": "cbed09ce8f85ecd5dc4b",
+    "url": "/Sendoh603/Static-react-practise2.git/static/js/runtime-main.c8c2a7f6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Sendoh603/Static-react-practise2.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Sendoh603/Static-react-practise2.git/precache-manifest.c6e1f5014ac36f37052d0c5ccb6eb759.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Sendoh603/Static-react-practise2.git/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
